b901190ef9885ecd797be84769b6879ab04ffea3
bokovushka <andriibokovyi@gmail.com>
2024-07-18 20:24:30 +0300

    update

--- src/js/modules/libs/with-lib-select2.js
+++ src/js/modules/libs/with-lib-select2.js
@@ -4,13 +4,26 @@ import "../../../../node_modules/select2/dist/js/select2.js";
 //? select
 $('.select-choose-specialist').select2({
 	minimumResultsForSearch: -1,
-	placeholder: 'Оберіть фахівця',
 	dropdownParent: $('.main')
+	// placeholder: 'Оберіть фахівця',
 });
 
 $('.select-choose-clinic').select2({
 	minimumResultsForSearch: -1,
-	placeholder: 'Оберіть клініку',
+	dropdownParent: $('.main')
+	// placeholder: 'Оберіть клініку',
+});
+
+$('.select-choose-specialist--popup').select2({
+	minimumResultsForSearch: -1,
+	dropdownCssClass: 'select-up-index',
+	dropdownParent: $('.main')
+	// placeholder: 'Оберіть фахівця',
+});
+
+$('.select-choose-clinic--popup').select2({
+	minimumResultsForSearch: -1,
+	dropdownCssClass: 'select-up-index',
 	dropdownParent: $('.main')
 });
 
