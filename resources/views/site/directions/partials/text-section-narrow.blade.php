ab0bfdca030ea78dea8d5ed2c2e47924ee716360
AndrewWebPhp <andreyss100@gmail.com>
2025-01-01 15:12:35 +0200

    changed narrow block height

--- resources/views/site/directions/partials/text-section-narrow.blade.php
+++ resources/views/site/directions/partials/text-section-narrow.blade.php
@@ -11,7 +11,7 @@
             </div>
         </div>
         <div class="col-12 col-md-6 col-xl-4 mb-5 mb-md-0">
-            <div class="bg-white rounded-sm p-3 p-md-6 d-flex flex-column min-h-500 h-0">
+            <div class="bg-white rounded-sm p-3 p-md-6 d-flex flex-column min-h-narrow-block h-0">
                 <div class="h3 font-weight-bolder text-blue mb-8 mb-md-5"></div>
                 <div class="content os-scrollbar-overflow">
                     {!! $data->text_one !!}
@@ -20,7 +20,7 @@
         </div>
     @else
         <div class="col-12 col-md-6 col-xl-4 mb-5 mb-md-0">
-            <div class="bg-white rounded-sm p-3 p-md-6 d-flex flex-column min-h-500 h-0">
+            <div class="bg-white rounded-sm p-3 p-md-6 d-flex flex-column min-h-narrow-block h-0">
                 <div class="h3 font-weight-bolder text-blue mb-8 mb-md-5"></div>
                 <div class="content os-scrollbar-overflow">
                     {!! $data->text_one !!}
@@ -28,7 +28,7 @@
             </div>
         </div>
         <div class="col-12 col-md-6 col-xl-4">
-            <div class="bg-white rounded-sm p-3 p-md-6 d-flex flex-column min-h-500 h-0">
+            <div class="bg-white rounded-sm p-3 p-md-6 d-flex flex-column min-h-narrow-block h-0">
                 <div class="h3 font-weight-bolder text-blue mb-8 mb-md-5"></div>
                 <div class="content os-scrollbar-overflow">
                     {!! $data->text_two !!}
