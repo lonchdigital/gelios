38d46eb7d1d45f93c16c2d253d1ae3fd4604e38d
AndrewWebPhp <andreyss100@gmail.com>
2025-04-07 11:52:40 +0300

    spaces for direction on mobile added

--- resources/views/site/directions/partials/text-section-narrow.blade.php
+++ resources/views/site/directions/partials/text-section-narrow.blade.php
@@ -27,7 +27,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-12 {{ ($hasChildren) ? 'col-xl-4':'col-xl-6' }} col-md-6 col-xl-4">
+        <div class="col-12 {{ ($hasChildren) ? 'col-xl-4':'col-xl-6' }} col-md-6 col-xl-4 mb-5 mb-md-0">
             <div class="bg-white rounded-sm p-3 p-md-6 d-flex flex-column min-h-narrow-block h-0">
                 <div class="h3 font-weight-bolder text-blue mb-8 mb-md-5"></div>
                 <div class="content rich-text-block os-scrollbar-overflow">
