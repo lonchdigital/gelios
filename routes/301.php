aceb96eac577d213a98d012018a1968816999791
AndrewWebPhp <andreyss100@gmail.com>
2025-06-11 13:44:42 +0300

    change 301 to 302

--- routes/301.php
+++ routes/301.php
@@ -299,7 +299,7 @@ $items = [
 if (!empty($items)) {
     foreach ($items as $item) {
         Route::get($item['from'], function () use ($item) {
-            return redirect()->away(config('app.url') . $item['to'], 301);
+            return redirect()->away(config('app.url') . $item['to'], 302);
         });
     }
 }
