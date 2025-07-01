bd86e4224fba437f2772da8dafb9cc64e9055a89
AndrewWebPhp <andreyss100@gmail.com>
2025-07-01 15:45:42 +0300

    UAH on en locale for all prices

--- resources/views/site/components/price.blade.php
+++ resources/views/site/components/price.blade.php
@@ -3,6 +3,10 @@
     @if($price->is_free)
         <div class="price">{{ trans('web.free') }}</div>
     @else
-        <div class="price">{{ $price->price }} грн</div>
+        @if( app()->getLocale() == 'en' )
+            <div class="price">{{ $price->price }} UAH</div>
+        @else
+            <div class="price">{{ $price->price }} грн</div>
+        @endif
     @endif
 </div>
